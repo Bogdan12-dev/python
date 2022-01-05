be5ebaa047140a07df4e953b020384eb9a6e13ac
Bogdan12-dev <55618988+Bogdan12-dev@users.noreply.github.com>
2022-01-05 14:18:50 +0200

    Дз номер 1
    Дз номер 1

---
+++ C__Users_HP_AppData_Local_Programs_Python_Python310.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+message = 'We all live in a yellow submarine'
+print(message)
+
+
+# In[2]:
+
+
+print("We all live in a yellow submarine")
+
+
+# In[3]:
+
+
+Message = 'Oh'
+print(Message)
+
+
+# In[4]:
+
+
+print("Oh")
+
+
+# In[ ]:
+
+
+
+
