13f5079a9f691bfba43cff40093a706ac11de603
Bogdan12-dev <55618988+Bogdan12-dev@users.noreply.github.com>
2022-02-04 16:06:03 +0200

    Дз4
    Дз4

---
+++ дз4.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[48]:
+
+
+number = [a for a in range(1,100)
+         if a%5 == 0 and a%7 == 0]
+print(number)
+
+
+# In[50]:
+
+
+number = [a for a in range(1,100)
+         if a%5 == 0 and a%7 == 0]
+print(number)
+sum(number)
+
+
+# In[3]:
+
+
+given_list = [False, 0, 'str', '123', 'hello', '%', 1.2, 1]
+for tuple_given_list in enumerate(given_list):
+    print(tuple)
+
+
+# In[4]:
+
+
+list = {1,2,3,4,5,6,7,8,9,0,14,15,14,18}
+list2 = {1,5,6,14,19,}
+spilni = list.intersection(list2)
+minimalni = min(list)
+minimalni2 = min(list2)
+max1 = max(list)
+max2 = max(list2)
+print(minimalni)
+print(minimalni2)
+print(max1)
+print(max2)
+print(spilni)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
